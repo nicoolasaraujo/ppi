b1d1bf3702ca035cab666c7061c0df587f85e3bf
pmenezesneto <pedroseverino@msn.com>
2017-11-15 19:45:01 -0200

    Add files via upload

--- view/galeria.php
+++ view/galeria.php
@@ -15,6 +15,7 @@ $activePage = 'galeria';
   <script src="../js/jquery-3.2.1.js"></script>
   <script src="../bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
   <script src="../js/galeria.js"></script>
+  
 </head>
 
 
@@ -23,25 +24,6 @@ $activePage = 'galeria';
   <?php include "navbar.php";?>
   <div class="container-fluid">
 
-
-
-
-    <!-- <nav class="navbar  navbar-light" style="background-color: #E2DEDD ;">
-    <div class="container-fluid">
-
-    <div class="navbar-header">
-    <a href="#" class="navbar-brand" style="color:black;">Clinica Medica</a>
-  </div>
-
-  <ul class="nav navbar-nav navbar navbar-light" style="background-color: #C6BEBD  ;">
-  <li><a href="index.php">Home</a></li>
-  <li class="active"><a href="#">Galeria </a></li>
-  <li><a href="#">Contato</a></li>
-  <li><a href="#">Agendamento</a></li>
-</ul>
-
-</div>
-</nav> -->
 
 <div class="container">
   <h1 class="h1">Galeria</h1>
@@ -88,22 +70,3 @@ $activePage = 'galeria';
 </body>
 
 </html>
-
-
-<!--   <table>
-<tbody>
-<tr>
-<td><img class="img-responsive" src="img/CasaFoto1.jpg" onmouseenter="onBorder(this)" onmouseleave="offBorder(this)" ></td>
-<td><img class="img-responsive" src="img/CasaFoto2.jpg" onmouseenter="onBorder(this)" onmouseleave="offBorder(this)"></td>
-<td><img class="img-responsive" src="img/CasaFoto3.jpg" onmouseenter="onBorder(this)" onmouseleave="offBorder(this)"></td>
-</tr>
-
-<tr>
-<td><img class="img-responsive" src="img/CasaFoto4.jpg" onmouseenter="onBorder(this)" onmouseleave="offBorder(this)"></td>
-<td><img class="img-responsive" src="img/CasaFoto5.jpg" onmouseenter="onBorder(this)" onmouseleave="offBorder(this)"></td>
-<td><img class="img-responsive" src="img/CasaFoto6.jpg" onmouseenter="onBorder(this)" onmouseleave="offBorder(this)"></td>
-
-</tr>
-</tbody>
-
-</table> -->
