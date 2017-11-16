9ac883a6781a6fb19b56dbdc5db03d269c48e349
nicolas <nogueira-nicolas@hotmail.com>
2017-11-16 01:15:52 -0200

    horarios

--- model/buscaHorarios.php
+++ model/buscaHorarios.php
@@ -1,6 +1,10 @@
 <?php
 
+class Hora{
+    public $hr;
+    
 
+}
 
 try
 {
@@ -8,48 +12,65 @@ try
   $conn = conectaAoMySQL();
 
   
-  $med = "";
+  $med = 0;
   $dt = "";
-  if (isset($_GET["med"]) &&isset($_GET["dt"]) )
+  if (isset($_GET["med"]) && isset($_GET["dt"]) )
   {
+
     $med = $_GET["med"];
     $dt = $_GET["dt"];
   }
     
   
-  $sql = $conn->prepare( "SELECT HORA FROM AGENDA WHERE COD_FUNCIONARIO = ? AND DATA = ?;");
+  $sql = $conn->prepare( "SELECT DISTINCT HORA FROM AGENDA WHERE COD_FUNCIONARIO = ? AND DATE(DATA) = DATE(?)");
 
 
   $sql->bind_param("is",$med,$dt);
+    // $sql->bind_param("i",$med);
+
 
   
   if (!$sql->execute())
     throw new Exception('Ocorreu uma falha ao buscar o endereco: ' . $conn->error);
   
+    // $jsonStr ="";
+    $hr=0;
 
-    $hr =0 ;
-
-  $sql->bind_result($hr);
-
+    $sql->bind_result($hr);
+    $results= "";
     $results = array();
 
-
+    $sql->store_result();
   if ($sql->num_rows > 0)
   {
 
-    while(($sql->fetch()){
-        array_push($results,$hr);
+    while($sql->fetch()){
+        // array_push($results,$hr);
+        $hora = new Hora();
+        $hora->hr = $hr;
+        $results[] = $hora;
+        $jsonStr = json_encode($results);
+        echo $jsonStr;   
     }
 
-    $jsonStr = json_encode($results);
-    echo $jsonStr;
     
-  }  
+  }else echo 'false';
+//   else {
+//     $hora = new Hora();
+//     $hora->hr = null;
+//     $result[] = $hora;
+
+//   }
+
+  
+  
+  
+  
 }
 catch (Exception $e)
 {
   $msgErro = $e->getMessage();
-//   echo "<script>alert('Erro')</script>";
+  echo "<script>alert('Erro')</script>";
   
 }
 
