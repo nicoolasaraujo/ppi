36007a1c3c8bca806c0746df146390605cfd5134
nicolas <nogueira-nicolas@hotmail.com>
2017-11-22 01:58:16 -0200

    conexão novaCheap

--- model/conection.php
+++ model/conection.php
@@ -1,15 +1,15 @@
 <?php
-  define("HOST", "localhost");
-  define("USER", "root");
-  define("PASSWORD", "12345");
-  define("DATABASE", "trabson");
+  define("HOST", "50.62.177.216");
+  define("USER", "nova_cheap");
+  define("PASSWORD", "nova_cheap");
+  define("DATABASE", "nova_cheap");
 
   function conectaAoMySQL()
   {
     $conn = new mysqli(HOST, USER, PASSWORD, DATABASE);
     if ($conn->connect_error)
-      throw new Exception('Falha na conexão com o MySQL: ' . $conn->connect_error);
-    else {
+      throw new Exception('Falha na conex達o com o MySQL: ' . $conn->connect_error);
+	else {
       $conn->set_charset("utf8");
     }
     return $conn;
